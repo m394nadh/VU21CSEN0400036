b698c8a1cf4fea72fd7b368b08001755d03d43ad
Dattatreyadonga <95728528+m394nadh@users.noreply.github.com>
2024-08-11 10:58:07 +0530

    Update 50 stars.cpp

--- 50 stars.cpp
+++ 50 stars.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 int main()
 {
-	int i=1,n ;
-	scanf("%d",&n);
-	while (i<=n)
-	{
-		printf("*",n);
-		i++;
-		
-	}
+    int i = 0, n = 50;  
+
+    while (i < n)
+    {
+        printf("*"); 
+        i++;
+    }
+
+    return 0;
 }
