e8aa0ff7b71901af8139c063aec57d57dc2932bc
Dattatreyadonga <95728528+m394nadh@users.noreply.github.com>
2024-08-11 11:02:57 +0530

    Update CSE36.cpp

--- CSE36.cpp
+++ CSE36.cpp
@@ -1,35 +1,42 @@
+
+
 #include <stdio.h>
-main()
-{
-	char ch;
-    int Q,T;
-    printf("my resturant menu\n");
-    scanf("%c",&ch);
-    printf("enter the quantity reuired of your food \n");
-    scanf("%d",&Q);
+
+int main(){
+    char ch;
+    int Q, T = 0
+    printf("My restaurant menu\n");
+    printf("Enter the food item (B for Burger, S for Sandwich, P for Pizza, F for French Fries): ");
+    scanf(" %c", &ch);  
+    printf("Enter the quantity required of your food: ");
+    scanf("%d", &Q);
+
     switch(ch)
     {
-    	case 'B':
-    	printf("you selected for burger");
-		T = Q*200;
-	     printf("the total bill = %d");
-		 break;
-		 case 'S':
-		 printf("you selected for sandwich");
-		 T = Q*150;
-		 printf("the total bill = %d");
-		 break;
-		 case 'P':
-		 printf("you selected pizza");
-		 T = Q*500;
-		 printf("the total bill = %d");	
-		 break;
-		 case 'F':
-		 printf("you selected french fries ");
-		 T = Q*50;
-		 printf("the total bill = %d");
-		 default :
-		 printf("enter the one is invalid");
-	}
-	
+        case 'B':
+            printf("You selected Burger\n");
+            T = Q * 200;
+            printf("The total bill = %d\n", T);
+            break;
+        case 'S':
+            printf("You selected Sandwich\n");
+            T = Q * 150;
+            printf("The total bill = %d\n", T);
+            break;
+        case 'P':
+            printf("You selected Pizza\n");
+            T = Q * 500;
+            printf("The total bill = %d\n", T);
+            break;
+        case 'F':
+            printf("You selected French Fries\n");
+            T = Q * 50;
+            printf("The total bill = %d\n", T);
+            break;
+        default:
+            printf("Invalid selection\n");
+            break;
+    }
+
+    return 0;  
 }
